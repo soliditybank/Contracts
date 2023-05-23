8978adb4c814a472815367d95a211ef04ce52d66
soliditybank.eth <109216723+soliditybank@users.noreply.github.com>
2023-05-23 22:46:08 +0200

    Update IERC20.sol

--- IERC20.sol
+++ IERC20.sol
@@ -1,20 +1,20 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity 0.8.16;
 
 interface IERC20 {
 
-	event Transfer(address indexed from, address indexed to, uint256 value);
-	event Approval(address indexed owner, address indexed spender, uint256 value);
+	event Transfer(address indexed from, address indexed to, uint256 amount);
+	event Approval(address indexed owner, address indexed spender, uint256 amount);
 
 	function name() external view returns (string memory);
 	function symbol() external view returns (string memory);
 	function decimals() external view returns (uint8);
 	function totalSupply() external view returns (uint256);
-	function balanceOf(address owner) external view returns (uint256 balance);
-	function transfer(address to, uint256 value) external returns (bool success);
-	function transferFrom(address from, address to, uint256 value) external returns (bool success);
-	function approve(address spender, uint256 value) external returns (bool success);
-	function allowance(address owner, address spender) external view returns (uint256 remaining);
+	function balanceOf(address owner) external view returns (uint256);
+	function transfer(address to, uint256 amount) external returns (bool);
+	function transferFrom(address from, address to, uint256 amount) external returns (bool);
+	function approve(address spender, uint256 amount) external returns (bool);
+	function allowance(address owner, address spender) external view returns (uint256);
 
 }
