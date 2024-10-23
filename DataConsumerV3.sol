15c65b85ede16c10c56fa1677b036971880f49b9
soliditybank.eth <109216723+soliditybank@users.noreply.github.com>
2024-10-23 23:46:52 +0200

    Create DataConsumerV3.sol

---
+++ DataConsumerV3.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+contract DataConsumerV3 {
+    AggregatorV3Interface internal dataFeed;
+
+    /**
+     * Network: Sepolia
+     * Aggregator: ETH/USD
+     * Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+     */
+    constructor() {
+        dataFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+    }
+
+    /**
+     * Returns the latest answer.
+     */
+    function getChainlinkDataFeedLatestAnswer() public view returns (int) {
+        (int answer) = dataFeed.latestRoundData();
+        return answer;
+    }
+}
